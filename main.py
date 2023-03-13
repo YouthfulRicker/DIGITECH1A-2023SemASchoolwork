6655d8bacb1464c8d92ecc9b9014acb9bfb38c8f
antariksh <antariksh@anand.nz>
2023-03-13 23:33:42 +0000

    add directory navigator in python

---
+++ main.py
@@ -0,0 +1,5 @@
+file = input("Which file would you like to run?\n1 - if-else.py (clubbing\n2 - loop.py (are we there yet)\n")
+if file == "1":
+  exec(open("1.6.1/if-else.py").read())
+elif file == "2":
+  exec(open("1.6.1/loop.py").read())
