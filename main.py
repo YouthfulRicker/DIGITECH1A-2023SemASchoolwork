74ec8a6a14d6b476098e41251ac89b0241b727b6
antariksh <antariksh@anand.nz>
2023-03-13 23:44:13 +0000

    bracket fix main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-file = input("Which file would you like to run?\n1 - if-else.py (clubbing\n2 - loop.py (are we there yet)\n")
+file = input("Which file would you like to run?\n1 - if-else.py (clubbing)\n2 - loop.py (are we there yet)\n")
 if file == "1":
   exec(open("1.6.1/if-else.py").read())
 elif file == "2":
