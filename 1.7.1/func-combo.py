f7dd9be30c96f98e08570bf1eb51b5ad83662e1b
antariksh <antariksh@anand.co.nz>
2023-03-20 22:53:30 +0000

    init py add, init sh add, func-combo experiments

---
+++ 1.7.1/func-combo.py
@@ -0,0 +1,10 @@
+import os
+def current_path(): 
+  print("Current working directory: " + os.getcwd()) 
+current_path()
+#os.chdir("/home/runner/DIGITECH1A/1.6.1/")
+os.chdir("1.6.1/")
+current_path()
+from 1.6.1 import loop
+
+#areWeThereYet()
---
+++ init.sh
@@ -0,0 +1,2 @@
+git config --global user.email "antariksh@anand.co.nz"
+git config --global user.name "antariksh"
