ce510dbfa874e74871d5cfea861a47784028050f
antariksh <antariksh@anand.co.nz>
2023-03-20 23:14:48 +0000

    ITS WORKING NOW MUAHAHAHAHAH

--- 1.7.1/func-combo.py
+++ 1.7.1/func-combo.py
@@ -1,10 +1,15 @@
 import os
-def current_path(): 
-  print("Current working directory: " + os.getcwd()) 
-current_path()
-#os.chdir("/home/runner/DIGITECH1A/1.6.1/")
-os.chdir("1.6.1/")
-current_path()
-from 1.6.1 import loop
+import sys
 
-#areWeThereYet()
+def current_path(): 
+    print("Current working directory: " + os.getcwd()) 
+
+#current_path()
+os.chdir("1.6.1/")
+#current_path()
+sys.path.append(os.getcwd())
+
+from loop import areWeThereYet
+from i\f\-el\se import areWeThereYet
+
+areWeThereYet()
