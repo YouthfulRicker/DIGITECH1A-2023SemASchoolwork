8a814d60dec7da0e201196e4a3526fcee6b64b14
antariksh <antariksh@anand.co.nz>
2023-03-20 23:07:16 +0000

    various fixes

--- 1.6.1/loop.py
+++ 1.6.1/loop.py
@@ -1,19 +1,16 @@
 awtystat = False
-def areWeThereYet():
-  areWeThereYet = input("Are we there yet?\n")
-  if areWeThereYet == "yes" or areWeThereYet == "y" or areWeThereYet == "ye" or areWeThereYet == "yep" or areWeThereYet == "Yes" or areWeThereYet == "Y" or areWeThereYet == "Ye" or areWeThereYet == "Yep" :
-    print("WHOHOOOOO, YIPPEE, A TRIP YEEHEEEEEEE")
-    awtystat = True
-    quit()
-  elif areWeThereYet == "no" or areWeThereYet == "n" or areWeThereYet == "nu" or areWeThereYet == "nope" or areWeThereYet == "No" or areWeThereYet == "N" or areWeThereYet == "Nu" or areWeThereYet == "Nope" :
-    print("THEN WHEN?!?!?!?!")
-    awtystat = False
-  else:
-    print("That's not an answer goddamnit.")
-#def checkAwtystat():
-#  if awtystat == False:
-#    areWeThereYet()
 
-#checkAwtystat()
-while awtystat == False:
-  areWeThereYet()
+def areWeThereYet():
+    global awtystat
+    while not awtystat:
+        answer = input("Are we there yet?\n").lower()
+        if answer in ["yes", "y", "ye", "yep"]:
+            print("WHOHOOOOO, YIPPEE, A TRIP YEEHEEEEEEE")
+            awtystat = True
+        elif answer in ["no", "n", "nu", "nope"]:
+            print("THEN WHEN?!?!?!?!")
+            awtystat = False
+        else:
+            print("That's not an answer goddamnit.")
+
+areWeThereYet()
