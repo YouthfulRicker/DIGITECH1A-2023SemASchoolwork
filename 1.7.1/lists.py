e6d6b9f1fa3e3659b4375f28872cae39464fdcf1
antariksh <antariksh@anand.co.nz>
2023-03-21 20:48:20 +0000

    lists.py update

---
+++ 1.7.1/lists.py
@@ -0,0 +1,9 @@
+from time import sleep
+listTest = [2, 5, 8, "good mornenko user", False]
+print("Hmmm, I wonder what's in my python list?")
+sleep(3)
+print(listTest[0]+listTest[2], "is in my python list apparently!\nLet me check real quick.")
+sleep(4)
+print("Here's my list: " + str(listTest))
+sleep(2)
+print("WAIT,", listTest[0]+listTest[2], ".isn't in there!!!")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-file = input("Which file would you like to run?\n1 - ifelse.py (clubbing)\n2 - loop.py (are we there yet)\n3 - func-combo.py (function combination)\n")
+file = input("Which file would you like to run?\n1 - ifelse.py (clubbing)\n2 - loop.py (are we there yet)\n3 - func-combo.py (function combination)\n4 - lists.py (experimenting with lists)\n")
 if file == "1":
   exec(open("1.6.1/ifelse.py").read())
 elif file == "2":
   exec(open("1.6.1/loop.py").read())
 elif file == "3":
   exec(open("1.7.1/func-combo.py").read())
+elif file == "4":
+  exec(open("1.7.1/lists.py").read())
 else:
   print('bruh try again.')
