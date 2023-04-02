294cf94eae93831b0bcd3b03e9c296d7a07d1146
antariksh <antariksh@anand.co.nz>
2023-04-02 21:38:49 +0000

    tryexcept tagging

--- 1.8.1/TryExceptExample.py
+++ 1.8.1/TryExceptExample.py
@@ -1,11 +1,13 @@
 varOne = "hutt"
 try:
   print(varOne)
-  try:
+  try: #try does the program
     varOne = 1
-  except:
+  except: #except is a failure message
     print("Something went wrong when printing")
-  finally:
+  else:
+    print('You are oddly competent!')
+  finally: #finally will happen regardless of anything
     print('Finally Executed')
 except:
   print("Something went wrong with everything")
