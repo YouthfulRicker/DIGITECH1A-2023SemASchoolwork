141f854d9c6fc146c5cdc0ab451d64dbf8115c03
antariksh <antariksh@anand.co.nz>
2023-04-02 21:37:20 +0000

    numbrguessr add

---
+++ numbrguessr.py
@@ -0,0 +1,20 @@
+#startDate: 2023.4.3
+
+#import time, random
+import random
+import time
+
+#intro
+
+#name + personalisation
+name = input("Well hello there! What is your name?\n")
+sleep(3)
+print(name, "What a nice name! Glad to have you here")
+
+#level start
+
+
+#number guessing
+
+
+#replay/quit
