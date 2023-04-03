7aa6833ad823f37caaa99419d3e2e0945fffc07b
antariksh <antariksh@anand.co.nz>
2023-04-03 23:27:48 +0000

    formatting

--- 1.6.1/loop.py
+++ 1.6.1/loop.py
@@ -1,18 +1,16 @@
 awtystat = False
 
-
 def areWeThereYet():
-  global awtystat
-  while not awtystat:
-    answer = input("Are we there yet?\n").lower()
-    if answer in ["yes", "y", "ye", "yep"]:
-      print("WHOHOOOOO, YIPPEE, A TRIP YEEHEEEEEEE")
-      awtystat = True
-    elif answer in ["no", "n", "nu", "nope"]:
-      print("THEN WHEN?!?!?!?!")
-      awtystat = False
-    else:
-      print("That's not an answer goddamnit.")
-
+    global awtystat
+    while not awtystat:
+        answer = input("Are we there yet?\n").lower()
+        if answer in ["yes", "y", "ye", "yep"]:
+            print("WHOHOOOOO, YIPPEE, A TRIP YEEHEEEEEEE")
+            awtystat = True
+        elif answer in ["no", "n", "nu", "nope"]:
+            print("THEN WHEN?!?!?!?!")
+            awtystat = False
+        else:
+            print("That's not an answer goddamnit.")
 
 areWeThereYet()
